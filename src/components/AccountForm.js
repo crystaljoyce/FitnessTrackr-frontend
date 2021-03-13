7ce2e7a5d216638683c4858e7f974f36c4ba041e
samrunyan5 <samjorzak@gmail.com>
2021-03-13 14:11:46 -0600

    Starting code for AccountForm

---
+++ src/components/AccountForm.js
@@ -0,0 +1,8 @@
+import React, {useState} from 'react';
+import {Link, useHistory} from 'react-router-dom';
+
+const AccountForm = () => {
+    return <div>hello from AccountForm</div>
+}
+
+export default AccountForm;
